68dbea14935bb4e57449668b26590aa368e0e959
urtoosloww <v40565@gmail.com>
2025-03-22 13:01:53 -0700

    3/22(computer guessing game)
    we made a lot of changes

--- main#2.py
+++ main#2.py
@@ -1,9 +1,27 @@
 from random import randint
-print("give me a number and I'll guess it")
-random = input()
-computer_guess = randint(1,100)
+from random import random as r
+maxnum = 100
+print("give me a number, 1-" + str(maxnum) + "and I'll guess it")
+ans = input()
+ans = int(ans)
+computer_guess = randint(1,maxnum)
+lownum = 0
+highnum = maxnum
 # print(computer_guess)
-clue = input("Is this hot or cold, " + str(computer_guess) + ": ")
-if clue == "hot":
-    #generate a number between computer_guess, and some higher number or something
-    print(randint(computer_guess))
+while 1:
+    clue = input("Is this hot or cold " + str(computer_guess) + ": ")
+    if computer_guess == ans:
+        print("you got it!!!")
+        break
+    if clue == "hot":
+        lownum = computer_guess - 10
+        highnum = computer_guess + 10
+        #generate a number between computer_guess, and some higher number or something
+        computer_guess = randint(lownum, highnum)
+    elif clue == "cold":
+        lownum += 10
+        highnum -= 10
+        computer_guess = (randint(lownum, highnum))
+        print(computer_guess)
+
+
