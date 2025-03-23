3fac301c2f1d5890f30aaa506035a46efe9610aa
urtoosloww <v40565@gmail.com>
2025-03-23 16:51:52 -0700

    Update main#2.py

--- main#2.py
+++ main#2.py
@@ -1,27 +1,24 @@
 from random import randint
 from random import random as r
+
 maxnum = 100
-print("give me a number, 1-" + str(maxnum) + "and I'll guess it")
+print("Give me a number, 1-" + str(maxnum) + " and I'll guess it")
 ans = input()
 ans = int(ans)
-computer_guess = randint(1,maxnum)
-lownum = 0
+computer_guess = randint(1, maxnum)
+lownum = 1
 highnum = maxnum
-# print(computer_guess)
-while 1:
+
+while True:
     clue = input("Is this hot or cold " + str(computer_guess) + ": ")
-    if computer_guess == ans:
-        print("you got it!!!")
+    if clue == ans:
+        print("You got it!!!")
         break
     if clue == "hot":
-        lownum = computer_guess - 10
-        highnum = computer_guess + 10
-        #generate a number between computer_guess, and some higher number or something
+        lownum = max(1, computer_guess - 10)  # Ensure lownum is at least 1
+        highnum = min(maxnum, computer_guess + 10)  # Ensure highnum doesn't exceed maxnum
         computer_guess = randint(lownum, highnum)
     elif clue == "cold":
-        lownum += 10
-        highnum -= 10
-        computer_guess = (randint(lownum, highnum))
-        print(computer_guess)
-
-
+        lownum = min(maxnum, lownum + 10)  # Ensure lownum doesn't exceed maxnum
+        highnum = max(1, highnum - 10)  # Ensure highnum is at least 1
+        computer_guess = randint(lownum, highnum)
